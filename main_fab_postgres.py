64b888b534d4f5021ba6563915eb32e19f99a1a4
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2017-08-22 05:22:15 -0400

    added psycopy2 DatabaseErrror to the execpt sections of functions to catch that explicit type of error

--- main_fab_postgres.py
+++ main_fab_postgres.py
@@ -18,7 +18,7 @@ try:
     # create a psycopg2 cursor that can execute queries
     cursor = conn.cursor()
     # create a new table with a single column called "name"
-except Exception as e:
+except pg2.DatabaseError as e:
     print("Uh oh, can't connect. Invalid dbname, user or password?")
     print(e)
 
