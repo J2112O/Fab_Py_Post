2584bb36d13b951cc184169f37092bead47b002e
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2017-09-06 20:05:32 -0400

    Still a work in progress. Needing to use copy_expert() and having issues

--- copy_command.py
+++ copy_command.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3.6
 
+import psycopg2 as pg2
 import constants as cons # Importing the constants.py file
 import csv
+
+SQL = "COPY staging_assets(pipe_id, kind, pipe_jt_num, pipe_heat, pipe_length,\
+ pipe_wall_thickness, degree) FROM {} DELIMITER ',' CSV HEADER;".format(cons.staging_path)
+
+try:
+     # use our connection values to establish a connection
+     conn = pg2.connect(cons.connect_str)
+     # create a psycopg2 cursor that can execute queries
+     cursor = conn.cursor()
+     # create a new table with a single column called "name"
+except pg2.DatabaseError as e:
+     print("Uh oh, can't connect. Invalid dbname, user or password?")
+     print(e)
 
 # This variable holds the path that contains the csv file from constants.py
 path = cons.staging_path
 
-data_file = open(path, newline='') # Opening and striping any newlines
+# Opening and striping any newlines from the csv file to insert
+data_file = open(path, newline='')
 reader = csv.reader(data_file) # Creating a reader obj
-
 header = next(reader) # Skipping the header
+
 
 def copy_insert():
     data = []
@@ -27,3 +42,17 @@ def copy_insert():
         data.append([custom_pipe_id,pipe_kind,pipe_jt_num,pipe_ht,pipe_lt,
                     pipe_wt,pipe_deg])
     return data
+
+
+def main():
+    data_set = copy_insert()
+    #cursor.copy_from(data_set,'staging_assets', sep=',',null='\\N',
+    #                 size=8192,columns=cons.staging_tbl_rows)
+    with open(cons.staging_path) as f:
+        cursor.copy_expert(SQL,f)
+        conn.commit()
+        cursor.close()
+
+
+if __name__ == "__main__":
+    main()
