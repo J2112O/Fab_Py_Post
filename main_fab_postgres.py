1f2787f09a262ec4e35e6c36efdf9ee98ee50c3b
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2017-10-22 10:56:37 -0400

    Did a lot of work renaming and creating querys to fit with re-configured fab_tracking database. Also renamed constants.py file to db_connections instead.

--- main_fab_postgres.py
+++ main_fab_postgres.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3.6
 
 import psycopg2 as pg2
-import constants as cons # from the constants.py file
-import table_querys as tq # from the table_querys.py file
+import constants as cons  # from the constants.py file
+import table_querys as tq  # from the table_querys.py file
 # both of the below imports are for the google aa_sheets API
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -23,18 +23,21 @@ try:
     cursor = conn.cursor()
     # create a new table with a single column called "name"
 except pg2.DatabaseError as e:
-    print("Uh oh, can't connect. Invalid dbname, user or password?")
+    print("Can't connect. Invalid dbname, user or password?")
     print(e)
+
 
 def sht_aa():
     an_insert = [row for row in tq.get_all_attributes(cursor)]
     for x in an_insert:
         aa_sheet.insert_row(x, row_count + 1)
 
+
 def sht_ba():
     an_insert = [row for row in tq.get_bnd_assets(cursor)]
     for x in an_insert:
         ba_sheet.insert_row(x, row_count + 1)
+
 
 def sht_pa():
     an_insert = [row for row in tq.get_pup_assets(cursor)]
--- table_querys.py
+++ table_querys.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3.6
 
+pipe_select = """SELECT * FROM pipe_assets WHERE  kind = 'PIPE_JOINT';"""
+bend_select = """SELECT pa.custom_name, pa.joint_number, pa.heat, pa.pipe_length, pa.wall_thickness, pa.kind, b.degree
+                FROM pipe_assets AS pa
+                LEFT JOIN bend AS b
+                ON pa.custom_name = b.bend_id
+                WHERE pa.kind = 'BEND'
+                ORDER BY pa.custom_name ASC;"""
+
+unknown_select = """SELECT pa.custom_name, pa.joint_number, pa.heat, pa.pipe_length, pa.wall_thickness, unk.kind
+                FROM pipe_assets AS pa
+                LEFT JOIN unknown_assets AS unk
+                ON pa.custom_name = unk.unknown_id
+                WHERE pa.kind NOT IN ('BEND', 'PIPE_JOINT')
+                ORDER BY pa.custom_name ASC;"""
+
+
 # This function retrieves all records from the weld_log table.
 def get_weld_log(weld_cursor):
     # selection string for weld_log table.
@@ -14,35 +30,47 @@ def get_weld_log(weld_cursor):
         print("No records")
         print(e)
 
+
 # This function retrieves all records from the pup_assets table.
-def get_pup_assets(pup_cursor):
-    #selection string for pup_assets table.
-    pup_select = "SELECT * FROM pup_assets;"
+def get_pipe_assets(pipe_cursor):
     try:
-        pup_cursor.execute(pup_select)
-        pup_rows = pup_cursor.fetchall()
-        for row in pup_rows:
+        pipe_cursor.execute(pipe_select)
+        pipe_rows = pipe_cursor.fetchall()
+        for row in pipe_rows:
             print(row)
-            return pup_rows
+            return pipe_rows
     except Exception as e:
         print("No records")
         print(e)
 
+
 # This function retrieves all the records from the bend_assets table.
-def get_bnd_assets(bnd_cursor):
-    # selection string for the bnd_assets table;
-    bend_select = "SELECT * FROM bend_assets;"
+def get_bend_assets(bend_cursor):
     try:
-        bnd_cursor.execute(bend_select)
-        bnd_rows = bnd_cursor.fetchall()
-        for row in bnd_rows:
+        bend_cursor.execute(bend_select)
+        bend_rows = bend_cursor.fetchall()
+        for row in bend_rows:
             print(row)
-            return bnd_rows
+            return bend_rows
+    except Exception as e:
+        print("No records")
+        print(e)
+
+
+def get_unknown_assets(unk_cursor):
+    try:
+        unk_cursor.execute(unknown_select)
+        unk_rows = unk_cursor.fetchall()
+        for row in unk_rows:
+            print(row)
+            return unk_rows
     except Exception as e:
         print("No records")
         print(e)
 
 # This function retrieves all the records from the all_attributes view.
+
+
 def get_aa_view(aa_view_cursor):
     # selection string for the all_attributes view.
     aa_select = "SELECT * FROM all_attributes;"
