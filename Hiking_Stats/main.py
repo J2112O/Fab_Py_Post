01b575c743e6070157af9890264b17bb3556a234
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2018-06-04 17:57:56 -0400

    Updating my helper command line script.

--- Hiking_Stats/main.py
+++ Hiking_Stats/main.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3.6
 
 import psycopg2 as pg2
-from creds import connect_str
+import creds as cr
 
-SQL = "SELECT * FROM shoe_brand;"
+shoe_labels = ("shoe_id", "name_brand",)
+trial_labels = ("trail_id", "name", "surface_material",)
+SHOE_SQL = "SELECT * FROM shoe_brand;"
+TRAIL_SQL = "SELECT * FROM trail_route"
 
-with pg2.connect(connect_str) as conn:
+with pg2.connect(cr.connect_creds) as conn:
     with conn.cursor() as curs:
-        curs.execute(SQL)
+        curs.execute(SHOE_SQL)
         shoe_rows = curs.fetchall()
+        print(shoe_labels)
         for row in shoe_rows:
+            print(row)
+        print(trial_labels)
+        curs.execute(TRAIL_SQL)
+        trail_rows = curs.fetchall()
+        for row in trail_rows:
             print(row)
